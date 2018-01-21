10ccb4093110fd8989397664b071fbcbeb656180
Dario <diort.2018@gmail.com>
2018-01-20 21:14:54 -0300

    Se agraron los colores y el boton para compartir

--- Scrips/Main.cs
+++ Scrips/Main.cs
@@ -1,22 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 public class Main : MonoBehaviour {
-	public Image press_img;
-	public Text pun_text, vel_text;
+	public Image press_img, der_img, izq_img, fle_img, win_img, vol_img, sha_img, vel_img;
+	public Text pun_text, vel_text, velf_text, sha_text, vol_text;
 	public Slider sld;
 	public Boton der_bot, izq_bot, start_bot, volver_bot, comp_bot;
 	public GameObject fin_obj;
 	public GameObject[] flechas;
+	public Color[] claro, oscuro, solido, victoria;
 	public Animator anim;
 	public float muestra, espera, min_t, ini_t;
 	public int ran, puntaje;
-	public bool apreta, over, presed, start, seguro;
+	public bool apreta, over, presed, start, seguro, isProcessing, isFocus;
 	void Start () {
-		espera = 0.25f;
-		muestra = 2;
 		sld.onValueChanged.AddListener(delegate {actualiza();});
+	}
+	IEnumerator ShareScreenshot(){
+		isProcessing = true;
+		yield return new WaitForEndOfFrame();
+		ScreenCapture.CaptureScreenshot("screenshot.png", 2);
+		string destination = Path.Combine(Application.persistentDataPath, "screenshot.png");
+		yield return new WaitForSecondsRealtime(0.3f);
+		if(!Application.isEditor){
+			AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+			AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+			intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+			AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+			AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + destination);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"),uriObject);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"),"Can you beat my score?");
+			intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+			AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser",intentObject, "Share your new score");
+			currentActivity.Call("startActivity", chooser);
+			yield return new WaitForSecondsRealtime(1);
+		}
+		yield return new WaitUntil(() => isFocus);
+		isProcessing = false;
 	}
 	IEnumerator empieza(){
 		seguro = true;
@@ -39,13 +63,27 @@ public class Main : MonoBehaviour {
 			puntaje++;
 			if(muestra > min_t){
 				muestra = muestra/1.02f;
-			}
-			//decrementa el tiempo
+			}//decrementa el tiempo
 			yield return new WaitForSecondsRealtime(espera);
 			StartCoroutine(lazo());
 		}else{
 			over = true;
 		}
+	}
+	void cambia_color(int num){
+		der_img.color = new Color(solido[num].r,solido[num].g,solido[num].b,der_img.color.a);
+		izq_img.color = new Color(solido[num].r,solido[num].g,solido[num].b,izq_img.color.a);
+		fle_img.color = new Color(oscuro[num].r,oscuro[num].g,oscuro[num].b,fle_img.color.a);
+		win_img.color = new Color(oscuro[num].r,oscuro[num].g,oscuro[num].b,win_img.color.a);
+		pun_text.color = new Color(victoria[num].r,victoria[num].g,victoria[num].b,pun_text.color.a);
+		velf_text.color = new Color(solido[num].r,solido[num].g,solido[num].b,velf_text.color.a);
+		vel_text.color = new Color(solido[num].r,solido[num].g,solido[num].b,vel_text.color.a);
+		sha_text.color = new Color(solido[num].r,solido[num].g,solido[num].b,sha_text.color.a);
+		vol_text.color = new Color(solido[num].r,solido[num].g,solido[num].b,vol_text.color.a);
+		vol_img.color = new Color(claro[num].r,claro[num].g,claro[num].b,vol_img.color.a);
+		sha_img.color = new Color(claro[num].r,claro[num].g,claro[num].b,sha_img.color.a);
+		vel_img.color = new Color(claro[num].r,claro[num].g,claro[num].b,vel_img.color.a);
+
 	}
 	void ejecuta(bool isder){
 		if(apreta){
@@ -80,8 +118,24 @@ public class Main : MonoBehaviour {
 			over = true;
 		}
 	}
-
+	public void ShareBtnPress(){
+		if(!isProcessing){
+			StartCoroutine(ShareScreenshot());
+		}
+	}
+	private void OnApplicationFocus (bool focus) {
+		isFocus = focus;
+	}
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){//rojo
+			cambia_color(0);
+		}
+		if(Input.GetKeyDown(KeyCode.W)){//verde
+			cambia_color(1);
+		}
+		if(Input.GetKeyDown(KeyCode.E)){//azul
+			cambia_color(2);
+		}
 		if(start){
 			if(der_bot.down){
 				ejecuta(true);
@@ -106,7 +160,7 @@ public class Main : MonoBehaviour {
 						StartCoroutine(lazo());
 					}
 					if(comp_bot.up){
-						Debug.Log("comparte");
+						ShareBtnPress();
 					}
 				}
 			}
